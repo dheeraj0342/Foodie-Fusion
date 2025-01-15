9d86faef656ce309aa1aaeecb9258f384277c84d
Yashsharma2004 <yashsharma2004.mtr@gmail.com>
2025-01-15 09:41:50 +0530

    Install Tailwind css and render Foodie Fusion

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center font-bold text-3xl'>Foodie Fusioin</div>
+  )
+}
+
+export default Home
