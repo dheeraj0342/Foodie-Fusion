7d0d9b295cecc31625bbedc00486c121f4b51e07
Yashsharma2004 <yashsharma2004.mtr@gmail.com>
2025-02-01 22:21:36 +0530

    Added Home page

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div className='flex justify-center items-center font-bold text-3xl'>Foodie Fusioin</div>
-  )
-}
-
-export default Home
